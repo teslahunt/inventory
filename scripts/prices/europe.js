5e20882a2bcbf34e02819c3bf49c3a520894abb7
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-05-15 16:08:06 +0200

    ci(scripts): omit Hungary because no euro currency

--- scripts/prices/europe.js
+++ scripts/prices/europe.js
@@ -4,9 +4,10 @@ const path = require('path')
 
 // only countries with EURO currency
 const {
-  cz, // Croatia  coin is not Euro
+  cz, // Croatia coin is not Euro
   se, // Sweden coin is not Euro
   dk, // Denmark coin is not Euro
+  hu, // Hungary coin is not not EU
   is, // Island is not EU
   ch, // Switzerland is not EU
   gb, // United Kingdom is not EU
