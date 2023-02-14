a76f579790ec2a36e63ea55751672ed7bc7bf5b4
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-02-15 00:38:33 +0100

    fix: paginate until get all the results

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 'use strict'
 
+const debug = require('debug-logfmt')('tesla-inventory')
 const inventories = require('./inventories')
+
+const timestamp = (start = process.hrtime.bigint()) => () =>
+  Math.round(Number(process.hrtime.bigint() - start) / 1e6) + 'ms'
 
 const got = require('got').extend({
   url: 'https://www.tesla.com/inventory/api/v1/inventory-results',
@@ -8,10 +12,15 @@ const got = require('got').extend({
   resolveBodyOnly: true
 })
 
-const toLowerCase = ({ results: items, total_matches_found: totalMatchesFound }) => ({
-  items,
-  total: Number(totalMatchesFound)
-})
+const toLowerCase = ({ results: items, total_matches_found: totalMatchesFound, ...rest }) => {
+  return {
+    items,
+    total: Number(totalMatchesFound)
+  }
+}
+
+const uniqBy = (arr, prop) =>
+  arr.filter((x, i, self) => i === self.findIndex(y => x[prop] === y[prop]))
 
 const ITEMS_PER_PAGE = 50
 
@@ -26,12 +35,14 @@ module.exports = async (inventory, opts, { headers, ...gotOpts } = {}) => {
     opts.model = `m${opts.model}`
   }
 
-  const paginate = (outsideOffset = 0) =>
+  const duration = timestamp()
+
+  const paginate = async (outsideOffset = 0) =>
     got({
       searchParams: {
         query: JSON.stringify({
           outsideOffset,
-          count: 0,
+          count: ITEMS_PER_PAGE,
           query: {
             ...inventoryProps,
             ...opts
@@ -42,22 +53,16 @@ module.exports = async (inventory, opts, { headers, ...gotOpts } = {}) => {
       headers: { 'user-agent': undefined, ...headers }
     }).then(toLowerCase)
 
-  const page = await paginate()
+  let offset = 0
+  let items = []
+  let page
 
-  const allPages = async () => {
-    const nRequests = Math.ceil(page.total / ITEMS_PER_PAGE) - 1
-    const offsets = [...Array(nRequests).keys()].map(n => (n + 1) * page.items.length)
-    const pages = await Promise.all(offsets.map(paginate))
-    return [page, ...pages].reduce((acc, page) => {
-      page.items.forEach(item => {
-        const isAlready = acc.some(({ VIN }) => VIN === item.VIN)
-        if (!isAlready) acc.push(item)
-      })
-      return acc
-    }, [])
-  }
+  do {
+    page = await paginate(offset)
+    items = uniqBy(items.concat(page.items), 'VIN')
+    offset = items.length
+    debug({ items: items.length, duration: duration() })
+  } while (page.items.length >= ITEMS_PER_PAGE)
 
-  const promise = page.total < ITEMS_PER_PAGE ? Promise.resolve(page.items) : allPages()
-
-  return promise.then(items => items.filter(item => item.Model === opts.model))
+  return items.filter(item => item.Model === opts.model)
 }
