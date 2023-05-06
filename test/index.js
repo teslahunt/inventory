2966e70b35f01dddac4591bb26cdbe0c24f16d4c
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-05-06 17:53:30 +0200

    test: add new and used cases (#20)
    * test: add new and used cases
    
    * refactor: merge got options

--- test/index.js
+++ test/index.js
@@ -44,7 +44,7 @@ test('ensure results are consistent', async t => {
   }
 })
 
-test('Model S', async t => {
+test('Model S used', async t => {
   const results = await teslaInventory('us', {
     condition: 'used',
     model: 's'
@@ -53,7 +53,16 @@ test('Model S', async t => {
   t.true(results.every(item => item.Model === 'ms'))
 })
 
-test('Model 3', async t => {
+test('Model S new', async t => {
+  const results = await teslaInventory('us', {
+    condition: 'new',
+    model: 's'
+  })
+
+  t.true(results.every(item => item.Model === 'ms'))
+})
+
+test('Model 3 used', async t => {
   const results = await teslaInventory('us', {
     condition: 'used',
     model: '3'
@@ -62,7 +71,16 @@ test('Model 3', async t => {
   t.true(results.every(item => item.Model === 'm3'))
 })
 
-test('Model X', async t => {
+test('Model 3 new', async t => {
+  const results = await teslaInventory('us', {
+    condition: 'new',
+    model: '3'
+  })
+
+  t.true(results.every(item => item.Model === 'm3'))
+})
+
+test('Model X used', async t => {
   const results = await teslaInventory('us', {
     condition: 'used',
     model: 'x'
@@ -71,9 +89,27 @@ test('Model X', async t => {
   t.true(results.every(item => item.Model === 'mx'))
 })
 
-test('Model Y', async t => {
+test('Model X new', async t => {
+  const results = await teslaInventory('us', {
+    condition: 'new',
+    model: 'x'
+  })
+
+  t.true(results.every(item => item.Model === 'mx'))
+})
+
+test('Model Y used', async t => {
   const results = await teslaInventory('us', {
     condition: 'used',
+    model: 'y'
+  })
+
+  t.true(results.every(item => item.Model === 'my'))
+})
+
+test('Model Y new', async t => {
+  const results = await teslaInventory('us', {
+    condition: 'new',
     model: 'y'
   })
 
