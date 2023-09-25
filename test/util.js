83b1d24503b16942bd8877113708e012a05dfb45
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-09-25 15:04:25 +0200

    fix: add error handler

--- test/util.js
+++ test/util.js
@@ -9,12 +9,19 @@ onExit(browser.close)
 const fetcher = async url => {
   const browserless = await browser.createContext()
 
-  const fn = browserless.evaluate(async (page, response) => response.text(), {
-    abortTypes: ['image', 'stylesheet', 'font', 'other'],
-    adblock: false,
-    waitUntil: 'networkidle0',
-    animations: true
-  })
+  const fn = browserless.evaluate(
+    async (page, response, error) => {
+      if (!error) return response.text()
+      console.error(error)
+      throw error
+    },
+    {
+      abortTypes: ['image', 'stylesheet', 'font', 'other'],
+      adblock: false,
+      waitUntil: 'networkidle0',
+      animations: true
+    }
+  )
 
   const result = await fn(url)
   await browserless.destroyContext()
