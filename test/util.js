57c2d8db33ded2e4072c17c7e90041d7075c0575
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-12-20 20:02:53 +0100

    test: simplify setup

--- test/util.js
+++ test/util.js
@@ -1,22 +1,23 @@
 'use strict'
 
-const uniqueRandomArray = require('unique-random-array')
-const userAgents = require('top-user-agents')
 const createBrowser = require('browserless')
 const { onExit } = require('signal-exit')
-const puppeteer = require('puppeteer')
 
-const randomUserAgent = uniqueRandomArray(userAgents)
-
-const browser = createBrowser({ puppeteer })
-
-onExit(browser.close)
+const browser = () =>
+  Promise.resolve(
+    this.instance ||
+      (this.instance = (() => {
+        const browser = createBrowser({ puppeteer: require('puppeteer') })
+        onExit(browser.close)
+        return browser
+      })())
+  ).then(browser => browser.createContext())
 
 const fetcher = async url => {
-  const browserless = await browser.createContext()
+  const browserless = await browser()
 
   const fn = browserless.evaluate(
-    async (page, response, error) => {
+    async (_, response, error) => {
       if (error) throw error
       const status = response.status()
       if (response.status() !== 200) throw new TypeError(`Response status: ${status}`)
@@ -26,8 +27,7 @@ const fetcher = async url => {
       abortTypes: ['image', 'stylesheet', 'font', 'other'],
       adblock: false,
       waitUntil: 'networkidle0',
-      animations: true,
-      headers: { 'user-agent': randomUserAgent() }
+      animations: true
     }
   )
 
